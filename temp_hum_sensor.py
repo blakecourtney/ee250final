b84291ee21ece4cbab5a3e8032bf72dd9e72e1fb
blakecourtney <bcourtne@usc.edu>
2024-11-26 23:02:34 -0500

    mini sensor firmware

--- temp_hum_sensor.py
+++ temp_hum_sensor.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 
 sys.path.append('~/Software/Python/')
 sys.path.append('~/Software/Python/grove_rgb_lcd')
-sys.path.append('~/Software/Python/grove_i2c_temp_hum_hdc1000')
+sys.path.append('~/Software/Python/grove_i2c_temp_hum_sensor_mini')
 
 import time
 # By appending the folder of all the GrovePi libraries to the system path here,
@@ -12,11 +12,18 @@ import time
 
 import grovepi
 from grove_rgb_lcd import *
-from grove_i2c_temp_hum_hdc1000 import *
-from grove_i2c_temp_hum_hdc1000 import HDC1000
+from grove_i2c_temp_hum_mini.py import *
+from grove_i2c_temp_hum_mini.py import grove_i2c_temp_hum_mini.py
+#from grove_i2c_temp_hum_hdc1000 import *
+#from grove_i2c_temp_hum_hdc1000 import HDC1000
 
-hdc = HDC1000()
-hdc.Config()
+import grove_i2c_temp_hum_mini
+import time
+
+#hdc = HDC1000()
+#hdc.Config()
+
+t= grove_i2c_temp_hum_mini.th02()
 
 #clear lcd
 setText("TESTING")
@@ -26,13 +33,11 @@ while True:
     try:
         #read distance from ultrasonic
         #read potentiometer threshold
-        temp = hdc.Temperature()
-        hum = hdc.Humidity()
+        #temp = hdc.Temperature()
+        #hum = hdc.Humidity()
         #format text
-        print('Temp    : %.2f C' % hdc.Temperature())
-        print('Humidity: %.2f %%' % hdc.Humidity())
 
-        line1 = 'Temp    : %.2f C' % str(temp) + '\n' + 'Humidity: %.2f %%' % str(hum)
+        line1 = "Temp: %.2fC\tHumidity:%.2f" % (t.getTemperature(), t.getHumidity()), "%"
         setText_norefresh(line1)
 
     except IOError:
